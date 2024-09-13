330c629237c7690eac284f7a70897eb187584947
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-09-14 00:12:16 +0200

    Better loading skeleton for dataplot.tsx

--- frontend/src/app/liquidity/DataPlot.tsx
+++ frontend/src/app/liquidity/DataPlot.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import { SimpleDataPoint } from './page';
 import dynamic from 'next/dynamic'
 
-const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
+function DataPlotSkeleton() {
+  return (
+    <div className='w-full h-full pb-3 pt-1 animate-pulse bg-[#f0f0f0] flex items-center justify-center rounded-md'>
+      <span className='text-[#191919]'>Chart Loading...</span>
+    </div>
+  );
+}
+
+const Plot = dynamic(() => import('react-plotly.js'), { ssr: false, loading: () => <DataPlotSkeleton /> });
 
 type DataPlotProps = {
   ticker: string;
@@ -60,18 +68,11 @@ function DataPlot({ ticker, color, data }: DataPlotProps): React.ReactElement {
     responsive: true,
   };
   
-  if (data.length === 0) {
-    return (
-      <div className={`w-full ${ticker === 'LIQUIDITY' ? 'h-[80vh] bg-zinc-300' : 'h-[60vh] bg-zinc-300'} rounded animate-pulse`}></div>
-    );
-  }
-
   return (
     <div className={ticker === 'LIQUIDITY' ? `w-full h-[80vh]` : `w-full h-[60vh]`}>
-      <div className='h-full pb-3 pt-1'>
-
+      <div className='h-full'>
         <Plot 
-          className='w-full h-full' 
+          className='w-full h-full pb-3 pt-1' 
           data={[trace]} 
           layout={layout} 
           config={config} 
