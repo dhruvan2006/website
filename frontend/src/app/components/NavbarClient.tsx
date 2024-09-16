011f6cd64cecc83b7aa5daf33911b64d2a251301
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-09-16 21:34:58 +0200

    Fix navbar client urls

--- frontend/src/app/components/NavbarClient.tsx
+++ frontend/src/app/components/NavbarClient.tsx
@@ -28,7 +28,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
 
   useEffect(() => {
     const checkApiKey = async () => {
-      const response = await fetch(`http://localhost:8000/api/indicators/check_api_key`, {
+      const response = await fetch(`https://api.gnanadhandayuthapani.com/api/indicators/check_api_key`, {
         headers: { Authorization: `Bearer ${session.accessToken}` },
       });
       if (response.ok) {
@@ -42,7 +42,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
   }, [session]);
 
   const fetchKey = async () => {
-    const response = await fetch(`http://localhost:8000/api/indicators/generate_api_key`, {
+    const response = await fetch(`https://api.gnanadhandayuthapani.com/api/indicators/generate_api_key`, {
       headers: { Authorization: `Bearer ${session.accessToken}` },
     })
     if (!response.ok) throw new Error("Fetch api key failed");
@@ -53,7 +53,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
 
   const [showRegenToast, setShowRegenToast] = useState<boolean>(false);
   const generateApiKey = async () => {
-    const response = await fetch(`http://localhost:8000/api/indicators/generate_api_key`, {
+    const response = await fetch(`https://api.gnanadhandayuthapani.com/api/indicators/generate_api_key`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${session.accessToken}` },
     });
@@ -68,7 +68,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
   };
 
   const regenerateApiKey = async () => {
-    const response = await fetch(`http://localhost:8000/api/indicators/regenerate_api_key`, {
+    const response = await fetch(`https://api.gnanadhandayuthapani.com/api/indicators/regenerate_api_key`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${session.accessToken}` },
     });
