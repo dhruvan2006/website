820efa9fecd48818d44986d1a12ebf69bb6f02ba
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-09-17 22:23:09 +0200

    Responsive navbar contd

--- frontend/src/app/components/NavbarClient.tsx
+++ frontend/src/app/components/NavbarClient.tsx
@@ -128,9 +128,9 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
   return (
     <header className='bg-[#fff] lg:bg-[#fff]/75 lg:backdrop-blur-md text-[#191919] flex flex-col font-sans fixed top-0 left-0 right-0 z-50 border-b border-zinc-300'>
       <nav className='container mx-auto px-4 sm:px-8 lg:px-16'>
-        <div className='flex justify-between items-center'>
+        <div className='flex lg:justify-between items-center'>
           {/* Logo */}
-          <Link href="/">
+          <Link className='flex-1 lg:flex-grow-0' href="/">
             <div className='flex items-center'>
               <Image src="/logo.svg" alt="Logo" width={32} height={32} />
               <span className='text-xl font-bold ml-2'>Dhruvan.</span>
@@ -138,9 +138,9 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
           </Link>
           
           {/* Get API button for Mobile */}
-          {!session.user ? (
+          {!session?.user ? (
             <Link href="/login">
-              <button className="font-semibold bg-[#191919] hover:bg-[#474747] text-[#fff] transition duration-300 py-1.5 px-4 rounded-md">
+              <button className="hidden md:block lg:hidden me-8 font-semibold bg-[#191919] hover:bg-[#474747] text-[#fff] transition duration-300 py-1.5 px-4 rounded-md">
                 Get API
               </button>
             </Link>
@@ -382,7 +382,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
         </div>
 
         {/* Navbar Dropdown */}
-        <div className={`lg:hidden h-screen md:min-h-screen bg-[#fff] md:border-l md:border-zinc-300 w-full md:w-1/3 flex flex-col transition-all duration-300 ease-in-out md:absolute md:top-0 ${isMenuOpen ? 'md:right-0 flex h-[92vh]' : 'md:min-h-screen md:-right-96 overflow-hidden'}`}>
+        <div className={`lg:hidden h-0 md:min-h-screen bg-[#fff] md:border-l md:border-zinc-300 w-full md:w-1/3 flex flex-col transition-all duration-300 ease-in-out md:absolute md:top-0 ${isMenuOpen ? 'md:right-0 flex min-h-[89vh]' : 'md:min-h-screen md:-right-96 overflow-hidden min-h-0'}`}>
           
           {/* Nav close for md */}
           <div className='hidden md:block'>
@@ -392,7 +392,7 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
           </div>
           
           {/* Nav Links */}
-          <ul className='p-4 mb-auto overflow-y-scroll'>
+          <ul className='p-4 overflow-y-scroll'>
             <li className='py-2 border-b border-b-white'>
               <Link href='/liquidity' className='p-2 block hover:bg-blue-500 text-[#3b3b3b] hover:text-[#191919] hover:bg-white rounded-md transition-all duration-300 ease-in-out'>
                 Liquidity
@@ -452,13 +452,13 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
                 </button>
               </div>
               {dataSources.map((dataSource, idx) => (
-                <div key={idx} className={ `mt-2 ml-2 flex-1 px-4 first:pl-0 last:pr-0 ${isDatasourcesOpen ? 'block' : 'hidden'}`}>
+                <ul key={idx} className={ `mt-2 ml-2 flex-1 px-4 first:pl-0 last:pr-0 ${isDatasourcesOpen ? 'block' : 'hidden'}`}>
                   <li className={`w-full ${pathname.startsWith(`/datasources/${dataSource.url}`) ? '' : 'hover:bg-white text-[#3b3b3b]'} rounded-md py-1`}>
                   <Link href={`/datasources/${dataSource.url}`} className={`p-3 transition duration-300 ${pathname.startsWith(`/datasources/${dataSource.url}`) ? 'text-blue' : 'hover:text-[#000]'}`}>
                       {dataSource.name}
                   </Link>
                   </li>
-                </div>
+                </ul>
               ))}
             </li>
             <li className='py-2 border-b border-b-white'>
@@ -481,13 +481,13 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
                 </button>
               </div>
               {notebooks.notebooks.map((notebook, idx) => (
-                <div key={idx} className={ `mt-2 ml-2 flex-1 px-4 first:pl-0 last:pr-0 ${isResearchOpen ? 'block' : 'hidden'}`}>
+                <ul key={idx} className={ `mt-2 ml-2 flex-1 px-4 first:pl-0 last:pr-0 ${isResearchOpen ? 'block' : 'hidden'}`}>
                   <li className={`w-full ${pathname.startsWith(`/notebooks/${notebook.path}`) ? '' : 'hover:bg-white text-[#3b3b3b]'} rounded-md py-1`}>
                   <Link href={`/notebooks/${notebook.path}`} className={`p-3 transition duration-300 ${pathname.startsWith(`/notebooks/${notebook.path}`) ? 'text-blue' : 'hover:text-[#000]'}`}>
                       {notebook.name}
                   </Link>
                   </li>
-                </div>
+                </ul>
               ))}
             </li>
             <li className='py-2'>
@@ -498,18 +498,28 @@ export default function NavbarClient({ indicators, dataSources, notebooks, sessi
           </ul>
 
           {/* Navbar Footer */}
-          <div className='flex space-x-4 items-center border-t border-zinc-300 p-4 m-4 md:m-0'>
-            <a href="https://github.com/dhruvan2006" target="_blank" rel="noopener noreferrer">
-              <svg className="w-8 h-8 text-[#191919] hover:text-[#474747] transition duration-300" fill="currentColor" viewBox="0 0 24 24">
-                <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-              </svg>
-            </a>
-            {/* LinkedIn Logo */}
-            <a href="https://www.linkedin.com/in/dhruvan-gnanadhandayuthapani-3452661b6/" target="_blank" rel="noopener noreferrer">
-              <svg className="w-6 h-6 text-[#191919] hover:text-[#474747] transition duration-300" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-              </svg>
-            </a>
+          <div className='mt-auto flex flex-row justify-between border-t border-zinc-300 p-4'>
+            <div className='flex space-x-4 items-center'>
+              <a href="https://github.com/dhruvan2006" target="_blank" rel="noopener noreferrer">
+                <svg className="w-8 h-8 text-[#191919] hover:text-[#474747] transition duration-300" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
+                </svg>
+              </a>
+              {/* LinkedIn Logo */}
+              <a href="https://www.linkedin.com/in/dhruvan-gnanadhandayuthapani-3452661b6/" target="_blank" rel="noopener noreferrer">
+                <svg className="w-6 h-6 text-[#191919] hover:text-[#474747] transition duration-300" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                </svg>
+              </a>
+            </div>
+            {/* Login btn for mobile */}
+            {!session?.user ? (
+            <Link href="/login">
+              <button className="block md:hidden lg:hidden font-semibold bg-[#191919] hover:bg-[#474747] text-[#fff] transition duration-300 py-1.5 px-4 rounded-md">
+                Get API
+              </button>
+            </Link>
+          ) : ''}
           </div>
         </div>
 
