a632673426dee5f252b07eb2a296d46add1807a2
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-10-22 12:15:36 +0200

    Globally remove cache usage

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -4,11 +4,13 @@ export async function customFetch(url: string, options: RequestInit = {}) {
     const frontendKey = process.env.FRONTEND_KEY;
     const defaultHeaders = {
         'X-Frontend-Key': frontendKey as string,
+        'Cache-Control': 'no-store',
         ...options.headers,
     };
 
     return fetch(url, {
         ...options,
         headers: defaultHeaders,
+        cache: 'no-store',
     });
 }
