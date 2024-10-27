3b8c86c0ee5e29613a2db1476fd74e0e1a89beb7
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-10-27 12:21:33 +0100

    Better ticker serializer

--- backend/speedometer/serializers.py
+++ backend/speedometer/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import Ticker, TickerScore
 
+class ScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TickerScore
+        fields = ['date', 'score', 'last_updated']
+
 class TickerSerializer(serializers.ModelSerializer):
+    scores = ScoreSerializer(source='tickerscore_set', many=True, read_only=True)
+    
     class Meta:
         model = Ticker
-        fields = ['ticker']
+        fields = ['ticker', 'scores']
 
 class TickerScoreSerializer(serializers.ModelSerializer):
     ticker = TickerSerializer()
