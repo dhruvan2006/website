49eaf7c61e4421d9d20bc327137a381e28a1bc5e
Dhruvan Gnanadhandayuthapani <dhruvan2006@gmail.com>
2024-11-01 17:51:09 +0100

    Add caching for valuation views using redis

--- backend/valuation/views.py
+++ backend/valuation/views.py
@@ -1,9 +1,12 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Valuation, ValuationIndicator
 from .serializers import ValuationSerializer, ValuationIndicatorSerializer
 
 class ValuationView(APIView):
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
     def get(self, request):
         valuations = Valuation.objects.all().order_by('date')
         indicators = ValuationIndicator.objects.all()
