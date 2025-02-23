7671e62f62b4f413081bc826ef4d99b2d8f52e39
Dhruvan Gnanadhandayuthapani <d.gnanadhandayuthapani@student.tudelft.nl>
2025-02-23 16:24:42 +0100

    Add dark theme for loading spinner

--- frontend/src/app/(main)/loading.tsx
+++ frontend/src/app/(main)/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className='h-[88vh] flex items-center justify-center'>
-      <div className="animate-spin rounded-full h-32 w-32 border-8 border-black border-t-8 border-t-transparent"></div>
+    <div className='h-[88vh] flex items-center justify-center bg-white dark:bg-zinc-900'>
+      <div className="animate-spin rounded-full h-32 w-32 border-8 border-black dark:border-white border-t-8 border-t-transparent dark:border-t-transparent"></div>
     </div>
   );
 }
